3cdd2b9af7fb64272fd62f5809c4921f098187b3
abhishek <alphaq1205@gmail.com>
2020-03-26 13:20:31 +0530

    Realtime url tracking extension

---
+++ src/contentJsBcp.js
@@ -0,0 +1,8 @@
+console.log('content script working................')
+
+const para=document.getElementsByTagName('div')
+
+for(element of para){
+    console.log(element+'///////////////////')
+    //element.style['background-color']="#996d99"
+}
